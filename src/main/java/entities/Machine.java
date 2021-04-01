9d5024d221e6769375c85a7a8178610b90df2936
Mantas <gramolis1@gmail.com>
2021-04-01 16:59:22 +0300

    Connected H2 database and made Machine class as entity

--- src/main/java/entities/Machine.java
+++ src/main/java/entities/Machine.java
@@ -1,9 +1,24 @@
 package entities;
 
+import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
+import java.util.Objects;
 
+@Entity
+@NamedQueries({
+        @NamedQuery(name = "Player.findAll", query = "select a from Player as a")
+})
+@Table(name = "MACHINE")
 public class Machine implements Serializable {
-    String name;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Size(max = 50)
+    @Column(name = "NAME")
+    private String name;
 
     public Machine(String name) {
         this.name = name;
@@ -19,4 +34,27 @@ public class Machine implements Serializable {
     public void setName(String name) {
         this.name = name;
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /* TODO: implement equals and hashCode functions
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Machine machine = (Machine) o;
+        return Objects.equals(id, machine.id) && Objects.equals(name, machine.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+     */
 }
